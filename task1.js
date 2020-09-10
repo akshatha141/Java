e9f0df0408344f577c5975831b4eac6697d7f345
Akshatha <akshatha141@gmail.com>
2020-09-10 12:44:48 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,10 @@
+var weekday = { Monday, Tuesday, Wednesday, Thursday, Friday };
+var weekend = { Saturday, Sunday };
+Boolean(vacation);
+
+function sleepIn(weekday, vacation) {
+	console.log(!weekday || vacation);
+}
+
+sleepIn(Monday, false);
+KeyboardEvent
